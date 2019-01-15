dc4288cb661b370654e04ef284c1ce415b73cec4
j-kyoda <j.kyoda@gmail.com>
2019-01-15 22:31:23 +0900

    Change install version to Python3.7.2

--- install_python.sh
+++ install_python.sh
@@ -8,11 +8,11 @@ yum -y install sqlite-devel readline-devel tk-devel httpd-devel
 yum -y install mysql-devel gdbm-devel xz-devel
 yum -y install libffi-devel
 
-# install python 3.7.1
+# install python 3.7.2
 cd /usr/local/src
-curl -LO https://www.python.org/ftp/python/3.7.1/Python-3.7.1.tgz
-tar fxz Python-3.7.1.tgz
-cd Python-3.7.1
+curl -LO https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+tar fxz Python-3.7.2.tgz
+cd Python-3.7.2
 ./configure --prefix=/opt/python3.7 --with-ensurepip=install \
 --with-threads --enable-shared LDFLAGS='-Wl,-rpath=/opt/python3.7/lib'
 make
