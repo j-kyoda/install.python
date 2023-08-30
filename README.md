021133ff92a5c88d5a94453d3a453ea352057fff
j-kyoda <j.kyoda@gmail.com>
2023-08-31 08:43:57 +0900

    Change install version to python3.11.5

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ install.python
 
 python install script for debian11.
 
-This script install Python3.11.4.
+This script install Python3.11.5.
 
 
 How to install
--- install_python.sh
+++ install_python.sh
@@ -16,8 +16,8 @@ apt-get -y install libgdbm-dev
 apt-get -y install liblzma-dev
 apt-get -y install libffi-dev
 
-# install python 3.11.4
-VERSION=3.11.4
+# install python 3.11.5
+VERSION=3.11.5
 SHORT_VER=3.11
 
 cd /usr/local/src
