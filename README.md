403e9fc3a5dc3b4dd5d133c103a8d9f6974a25ec
j-kyoda <j.kyoda@gmail.com>
2025-02-07 21:50:10 +0900

    Change install version to python3.13.2

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ install.python
 
 python install script for debian12.
 
-This script install Python3.13.1.
+This script install Python3.13.2.
 
 
 How to install
--- install_python.sh
+++ install_python.sh
@@ -16,8 +16,8 @@ apt-get -y install libgdbm-dev
 apt-get -y install liblzma-dev
 apt-get -y install libffi-dev
 
-# install python 3.13.1
-VERSION=3.13.1
+# install python 3.13.2
+VERSION=3.13.2
 SHORT_VER=3.13
 
 cd /usr/local/src
