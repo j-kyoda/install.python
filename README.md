23514512a144c1ff70ed92af6d81d0ecc1389d03
j-kyoda <j.kyoda@gmail.com>
2025-10-15 09:30:24 +0900

    Change install version to python3.13.8

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ install.python
 
 python install script for debian12.
 
-This script install Python3.13.7.
+This script install Python3.13.8.
 
 
 How to install
--- install_python.sh
+++ install_python.sh
@@ -16,8 +16,8 @@ apt-get -y install libgdbm-dev
 apt-get -y install liblzma-dev
 apt-get -y install libffi-dev
 
-# install python 3.13.7
-VERSION=3.13.7
+# install python 3.13.8
+VERSION=3.13.8
 SHORT_VER=3.13
 
 cd /usr/local/src
