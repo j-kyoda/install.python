713c72a805ba901bcd236bdc2976045254108d28
j-kyoda <j.kyoda@gmail.com>
2026-02-07 10:24:13 +0900

    Change install version to python3.14.3

--- install_python.sh
+++ install_python.sh
@@ -16,8 +16,8 @@ apt-get -y install libgdbm-dev
 apt-get -y install liblzma-dev
 apt-get -y install libffi-dev
 
-# install python 3.14.2
-VERSION=3.14.2
+# install python 3.14.3
+VERSION=3.14.3
 SHORT_VER=3.14
 
 cd /usr/local/src
