cd0a5bb555239869a6239f70773daab2bb39706e
j-kyoda <j.kyoda@gmail.com>
2026-08-07 22:08:56 +0900

    Change install version to python3.14.7

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ install.python
 
 python install script for debian12.
 
-This script install Python3.14.6.
+This script install Python3.14.7.
 
 
 How to install
--- install_python.sh
+++ install_python.sh
@@ -16,8 +16,8 @@ apt-get -y install libgdbm-dev
 apt-get -y install liblzma-dev
 apt-get -y install libffi-dev
 
-# install python 3.14.6
-VERSION=3.14.6
+# install python 3.14.7
+VERSION=3.14.7
 SHORT_VER=3.14
 
 cd /usr/local/src
